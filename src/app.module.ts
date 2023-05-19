ed76233f84cfb54681ef2031b9e1ae0a32f3b4de
Papa Ibrahima SOW <papaibrahimasow@esp.sn>
2023-05-19 11:02:18 +0000

    enabling cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { WaterWqModule } from './water-wq/water-wq.module';
 
 @Module({
   imports: [
+    
     MongooseModule.forRoot('mongodb+srv://sowibou032:ibrahima12@cluster0.9egru8k.mongodb.net/'),
     // MongooseModule.forRoot('mongodb://localhost:27017'),
     WaterModule,
