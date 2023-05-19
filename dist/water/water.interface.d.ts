8463b7700a2e9ee69cdb97b8c9b782314a52ce4b
Papa Ibrahima SOW <papaibrahimasow@esp.sn>
2023-05-19 08:54:16 +0000

    removing dist

--- dist/water/water.interface.d.ts
+++
@@ -1,7 +0,0 @@
-export interface Water {
-    temperature_TT: string;
-    humidity: string;
-    proximity: string;
-    pression: string;
-    boxes: [string];
-}
--- dist/waterqt/waterqt.interface.d.ts
+++
@@ -1,7 +0,0 @@
-export interface Waterwq {
-    temperature_WQ: string;
-    turbidity: string;
-    conductivity: string;
-    ph: string;
-    oxygen: string;
-}
